0b0aace9286b782a0a03a04a20ed08634e1527bc
gryttr <heimrarnardalr@gmail.com>
2020-04-27 17:33:09 -0400

    Appropriate blocks can be tilled into farmland

--- src/main/java/net/heimrarnadalr/soilstairs/mixin/HoeMixin.java
+++ src/main/java/net/heimrarnadalr/soilstairs/mixin/HoeMixin.java
@@ -8,9 +8,12 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import net.heimrarnadalr.soilstairs.block.SoilStairsBlocks;
+import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
 import net.minecraft.block.SlabBlock;
 import net.minecraft.block.StairsBlock;
+import net.minecraft.block.enums.SlabType;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.HoeItem;
@@ -30,7 +33,8 @@ public class HoeMixin {
 		BlockPos pos = itemUsageContext.getBlockPos();
 		if (itemUsageContext.getSide() != Direction.DOWN && world.getBlockState(pos.up()).isAir()) {
 		 	BlockState bs = world.getBlockState(pos);
-		 	if (bs.getBlock() == SoilStairsBlocks.COARSE_DIRT_STAIRS) {
+		 	Block block = bs.getBlock();
+		 	if (block == SoilStairsBlocks.COARSE_DIRT_STAIRS) {
 		 		PlayerEntity player = itemUsageContext.getPlayer();
 				world.playSound(player, pos, SoundEvents.ITEM_HOE_TILL, SoundCategory.BLOCKS, 1.0F, 1.0F);
 				if (!world.isClient) {
@@ -47,7 +51,7 @@ public class HoeMixin {
 					}
 				}
 				info.setReturnValue(ActionResult.SUCCESS);
-		 	} else if (bs.getBlock() == SoilStairsBlocks.COARSE_DIRT_SLAB) {
+		 	} else if (block == SoilStairsBlocks.COARSE_DIRT_SLAB) {
 		 		PlayerEntity player = itemUsageContext.getPlayer();
 				world.playSound(player, pos, SoundEvents.ITEM_HOE_TILL, SoundCategory.BLOCKS, 1.0F, 1.0F);
 				if (!world.isClient) {
@@ -62,6 +66,20 @@ public class HoeMixin {
 					}
 				}
 				info.setReturnValue(ActionResult.SUCCESS);
+		 	} else if (block == SoilStairsBlocks.GRASS_SLAB || block == SoilStairsBlocks.GRASS_PATH_SLAB || block == SoilStairsBlocks.DIRT_SLAB) {
+		 		if (bs.get(SlabBlock.TYPE) == SlabType.DOUBLE) {
+			 		PlayerEntity player = itemUsageContext.getPlayer();
+					world.playSound(player, pos, SoundEvents.ITEM_HOE_TILL, SoundCategory.BLOCKS, 1.0F, 1.0F);
+					if (!world.isClient) {
+						world.setBlockState(pos, Blocks.FARMLAND.getDefaultState());
+						if (player != null) {
+							itemUsageContext.getStack().damage(1, (LivingEntity)player, (Consumer<LivingEntity>)((playerEntity_1x) -> {
+								(playerEntity_1x).sendToolBreakStatus(itemUsageContext.getHand());
+							}));
+						}
+					}
+					info.setReturnValue(ActionResult.SUCCESS);
+		 		}
 		 	}
 		}
 	}
